b6e293a3838be52a46d37fc2fe477be99ba342bd
huynh <huynh@PAIMON>
2023-03-08 14:22:41 +0700

    Add project files.

---
+++ WebApplication1/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public List<Category> Categories { get; private set; }
+
+        public ActionResult Create()
+        {
+            var viewModel = new CoursesController
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+
+    }
+}
